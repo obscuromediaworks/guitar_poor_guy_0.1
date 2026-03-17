4e8aa5ae69465ac20a60ffd47db359cae55375da
elroipa <obscuromediaworks@gmail.com>
2026-03-17 19:44:20 -0300

    Update .gitignore & Setup Wwise events

--- gpg_0.1_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ gpg_0.1_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,50 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_MUSIC_INCUBUS_PRIVILEGE = 1812711115U;
+        static const AkUniqueID PLAY_SFX_HIT_001 = 681554853U;
+        static const AkUniqueID PLAY_SFX_LANEPRESS = 1856456021U;
+        static const AkUniqueID PLAY_SFX_MISS_000 = 17380391U;
+        static const AkUniqueID PLAY_SFX_UI_CLICK = 3504421513U;
+        static const AkUniqueID STOP_MUSIC_INCUBUS_PRIVILEGE = 2659419889U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAMEFLOW
+        {
+            static const AkUniqueID GROUP = 392736999U;
+
+            namespace STATE
+            {
+                static const AkUniqueID GAMEPLAY = 89505537U;
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID PAUSE = 3092587493U;
+                static const AkUniqueID RESULTS = 3780578133U;
+            } // namespace STATE
+        } // namespace GAMEFLOW
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace HITQUALITY
+        {
+            static const AkUniqueID GROUP = 2990443U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GOOD = 668632890U;
+                static const AkUniqueID MISS = 3062523241U;
+                static const AkUniqueID PERFECT = 2161557176U;
+            } // namespace SWITCH
+        } // namespace HITQUALITY
+
+    } // namespace SWITCHES
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
@@ -19,7 +63,10 @@ namespace AK
 
     namespace BUSSES
     {
-        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+        static const AkUniqueID BUS_MASTER = 3964155266U;
+        static const AkUniqueID BUS_MUSIC = 1162281553U;
+        static const AkUniqueID BUS_SFX = 3895923845U;
+        static const AkUniqueID BUS_UI = 1746463750U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
